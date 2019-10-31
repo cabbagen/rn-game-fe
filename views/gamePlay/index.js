5218d214be0e386eb3da334b0fba7c73a85c66c8
cabbagen <754689243@qq.com>
2019-10-31 09:16:15 +0800

    添加web通信

--- views/gamePlay/index.js
+++ views/gamePlay/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { WebView } from 'react-native';
+import { WebView, AsyncStorage } from 'react-native';
 
 export default class LemmaGame extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -8,10 +8,40 @@ export default class LemmaGame extends Component {
     }
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: '',
+    };
+    this.webview = null;
+  }
+
+  componentWillMount() {
+    AsyncStorage.getItem('token').then(value => {
+      this.setState({ token: value })
+    })
+  }
+
   render() {
     const uri = this.props.navigation.getParam('uri', '');
+
+    if (!this.state.token) {
+      return null
+    }
+
     return (
-      <WebView useWebKit startInLoadingState source={{ uri }} />
+      <WebView
+        useWebKit
+        source={{ uri }}
+        javaScriptEnabled
+        startInLoadingState
+        ref={webview => this.webview = webview}
+        onLoadEnd={this.handleWebViewLoadEnd.bind(this)}
+      />
     );
+  }
+
+  handleWebViewLoadEnd() {
+    this.webview.postMessage(this.state.token);
   }
 }
